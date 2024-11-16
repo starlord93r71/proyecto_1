aa9b0ace8a162ff79e75593098e8c2954225a609
piter echeverry <pitermaicole@gmail.com>
2024-11-15 21:27:40 -0500

    primera versión

---
+++ script.js
@@ -0,0 +1,13 @@
+function pruebaAlcance(){
+    let mensaje = "!Hola desde dentro de la función";
+
+    if(true){
+        let saludo = "!Hola desde dentro del bloque if!"
+        alert(saludo)
+    }
+
+    alert(mensaje);
+    alert(saludo);
+}
+
+pruebaAlcance();
